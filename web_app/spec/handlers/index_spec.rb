368ed284219a86fd45e7c01ece91f109950a3840
Piotr Szmielew <p.szmielew@ava.waw.pl>
2019-03-31 19:36:04 +0200

    specs for handlers/index

--- web_app/spec/handlers/index_spec.rb
+++ web_app/spec/handlers/index_spec.rb
@@ -19,17 +19,49 @@ describe Handlers::Index do
   before do
     allow(database_mock).to receive(:dataset).and_return(dataset_mock)
     allow(adapter_class_mock).to receive(:new).and_return(adapter_mock)
-    allow(adapter_mock).to receive(:sanitized_params).and_return({})
+    allow(adapter_mock).to receive(:sanitized_params).and_return(params)
   end
 
-  describe '#call' do
+  describe '#locals' do
+
     describe 'with empty params' do
       let(:params) { {} }
 
-      it 'must return empty params' do
+      it 'must return not mutated dataset' do
         index_handler.call
-        # expect(index_handler.sanitized_params).to eq({})
+        expect(index_handler.locals).to eq({ dataset: dataset_mock })
+      end
+
+      it 'must call sanitized_params on adapter' do
+        expect(adapter_mock).to receive(:sanitized_params).and_return(params)
+        index_handler.call
       end
     end
+
+    describe 'when filtering with name' do
+      let(:params) { {"name" => "XYZ"} }
+
+      it 'must call filter_by_name on dataset' do
+        expect(dataset_mock).to receive(:filter_by_name).and_return(dataset_mock)
+        index_handler.call
+      end
+    end
+
+    describe 'when filtering with source' do
+      let(:params) { {"source" => "XYZ"} }
+
+      it 'must call filter_by_source on dataset' do
+        expect(dataset_mock).to receive(:filter_by_source).and_return(dataset_mock)
+        index_handler.call
+      end
+    end
+
+    # describe 'when filtering with date' do
+    #   let(:params) { {date: "XYZ"} }
+
+    #   it 'must return not mutated dataset' do
+    #     expect(index_handler.locals).to eq({ dataset: dataset_mock })
+    #   end
+    # end
   end
 end
